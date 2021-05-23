1ac38417717b890f4d0ad77789c81b4278c2dccb
LABCAT <shane.watters.nz@gmail.com>
2021-05-23 18:50:18 +1200

    Updating shapes and converting into random pattern.

--- src/js/P5Sketch.js
+++ src/js/P5Sketch.js
@@ -15,95 +15,118 @@ const P5Sketch = () => {
     p.canvasHeight = window.innerHeight;
 
     //https://coolors.co/0b3954-087e8b-64abbb-bfd7ea-df99a5-ff5a5f-e43c42-c81d25
-    p.colourPalette = [
-      "#0b3954",
-      "#087e8b",
-      "#64abbb",
-      "#bfd7ea",
-      "#df99a5",
-      "#ff5a5f",
-      "#e43c42",
-      "#c81d25",
-    ];
+    // p.colourPalette = [
+    //   "#0b3954",
+    //   "#087e8b",
+    //   "#64abbb",
+    //   "#bfd7ea",
+    //   "#df99a5",
+    //   "#ff5a5f",
+    //   "#e43c42",
+    //   "#c81d25",
+    // ];
+
+    p.colourPalette = ["#1f2041","#4b3f72","#ffc857","#119da4","#19647e","#007991","#439a86","#a1b5d8"];
+
 
     p.setup = () => {
       p.canvas = p.createCanvas(p.canvasWidth, p.canvasHeight);
+      p.colorMode(p.HSB, 360, 100, 100, 100);
+      p.noLoop();
     };
 
     p.draw = () => {
       p.background(255);
-      p.pill();
-      p.donut();
-      p.trippleHex();
-      p.infinity();
-      p.noLoop();
+      for(let i=0; i < 2000; i++){
+          let x = p.randomGaussian(0.5, 0.14) * p.width;
+          let y = p.randomGaussian(0.5, 0.14) * p.height;
+          let w = p.random(10, 100) * p.random(p.random(p.random()));
+          p.randomShape(x, y, w);
+      }
     };
 
+    p.randomShape = (x, y, w) => {
+      const probability = parseInt(p.random(200));
+      const angle = (parseInt(p.random(8)) * (p.TAU/8))-p.PI*0.05;
+      const shape = p.random(p.shapes);
+      let size = 0;
+      p.push();
+      p.translate(x, y);
+      p.rotate(angle);
+      switch (probability) {
+        case 0:
+          size = p.random(1, 5);
+          p.infinitySymbol(size, size);    
+          break;
+        case 1:
+          size = p.random(p.width / 16);
+          p.leaf(size);    
+          break;
+        case 2:
+          size = p.random(p.width / 16);
+          p.trippleHex(size);    
+          break;
+        default:
+          p.randomLine(w);
+          break;
+      }
+      p.pop();
+    }
 
-
-    p.infinity = () => {
-      p.translate(p.width / 2, p.height / 2);
-      p.stroke(p.random(p.colourPalette));
-      p.fill(p.random(p.colourPalette));
-      p.scale(2);
-      p.strokeWeight(4);      
-      p.beginShape();
-      p.curveVertex(0, 0);
-      p.curveVertex(55, 30);
-      p.curveVertex(77.5, 0);
-      p.curveVertex(55, -30);
-      // p.curveVertex(0 - 40, 0 - 40);
-      // p.curveVertex(0 - 40, 0 - 60);
-      // p.curveVertex(0 + 40, 0 - 40);
-      // p.curveVertex(0 + 40, 0 - 60);
-      // p.curveVertex(0 - 40, 0 + 40);
-      // p.curveVertex(0 - 40, 0 + 60);
-      // p.curveVertex(0 + 40, 0 + 40);
-      // p.curveVertex(0 + 40, 0 + 60);
-      
-      p.curveVertex(0, 0);
-      p.curveVertex(-55, 30);
-      p.curveVertex(-77.5, 0);
-      p.curveVertex(-55, -30);
-      p.curveVertex(0, 0);
-      p.endShape(p.CLOSE);
-      p.translate(-p.width / 2, -p.height / 2);
+    p.randomLine = (lenght) => {
+      const colour = p.color(p.random(p.colourPalette));
+      colour.setAlpha(p.random(100)*p.random(p.random()));
+      p.fill(colour);
+      p.stroke(colour);
+      p.line(0, 0, lenght * p.random(1, 20), 0);
     };
 
-    p.pill = () => {
-      p.translate(p.width / 4 * 3, p.height / 4 * 3);
+    p.infinitySymbol = (leftSize, rightSize, centerX = 0, centerY = 0) => {
       const colours = ShuffleArray(p.colourPalette);
-      let size = p.width / 10;
-      let distanceMultiplier = size/colours.length;
-      for (let i = 0; i < colours.length; i++) {
-        p.stroke(colours[i]);
-        p.fill(colours[i]);
-        p.rect(i * distanceMultiplier/4, i * distanceMultiplier/2, size/2, size, 20, 20);
-        size = size - distanceMultiplier; 
-      }
-      p.translate(-p.width / 4 * 3, -p.height / 4 * 3);
+      const strokeC = p.color(colours[0]);
+      const fillC = p.color(colours[colours.length - 1]);
+      strokeC.setAlpha(p.random(50, 400));
+      fillC.setAlpha(p.random(50, 400));
+      p.stroke(strokeC);
+      p.fill(fillC);
+      p.strokeWeight(8);      
+      p.beginShape();
+      p.curveVertex(centerX, centerY);
+      p.curveVertex(centerX, centerY);
+      p.curveVertex(6 * rightSize, 4 * rightSize);
+      p.curveVertex(9 * rightSize, 3 * rightSize);
+      p.curveVertex(10 * rightSize, 0);
+      p.curveVertex(9 * rightSize, -3 * rightSize);
+      p.curveVertex(6 * rightSize, -4 * rightSize);
+      p.curveVertex(centerX, centerY);
+      p.curveVertex(-6  * leftSize, 4 * leftSize);
+      p.curveVertex(-9  * leftSize, 3  * leftSize);
+      p.curveVertex(-10  * leftSize, 0);
+      p.curveVertex(-9  * leftSize, -3 * leftSize);
+      p.curveVertex(-6  * leftSize, -4 * leftSize);
+      p.curveVertex(centerX, centerY);
+      p.endShape(p.CLOSE);
     };
 
-    p.donut = () => {
-      p.translate(p.width / 4, p.height / 4);
-      for (let i = 0; i < 96; i++) {
-        p.fill(p.random(p.colourPalette));
-        p.ellipse(0, 100, 40, 80);
-        p.rotate(p.PI / 24);
+    p.leaf = (size) => {
+      const colours = ShuffleArray(p.colourPalette);
+      let count = 0;
+      while (count < size / 2) {
+        let colour = p.lerpColor(p.color(colours[0]), p.color(colours[colours.length - 1]), (1 / size) * count * 4)
+        p.fill(colour);
+        p.stroke(colour);
+        p.ellipse(count, count, size - (count * 2), size - (count * 2), 20, 20, 20, 20);
+        count++;
       }
-      p.translate(-p.width / 4, -p.height / 4);
     };
 
-    p.trippleHex = () => {
-      p.translate(p.width / 4 * 3, p.height / 4); 
-      let size = p.width / 10;
+    p.trippleHex = (size) => {
       for (let i = 0; i < 4; i++) {
         p.stroke(p.random(p.colourPalette));
         p.fill(p.random(p.colourPalette));
         p.hexagon(0, 0, size);
         size = size / 2;
       }
-      p.translate(-p.width / 4 * 3, -p.height / 4);
     };
 
     /*
